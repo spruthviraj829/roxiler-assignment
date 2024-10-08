d45e75459ef94d0eacadc352ca157ca4428adc84
pruthviraj <spruthviraj829@gmail.com>
2024-10-09 00:06:23 +0530

    code added

---
+++ frontend/components/MonthSelect.jsx
@@ -0,0 +1,39 @@
+
+
+// eslint-disable-next-line react/prop-types
+const MonthSelector = ({ setSelectedMonth, selectedMonth }) => {
+    console.log('setSelectedMonth:', setSelectedMonth);
+  const months = [
+    { value: 1, label: 'January' },
+    { value: 2, label: 'February' },
+    { value: 3, label: 'March' },
+    { value: 4, label: 'April' },
+    { value: 5, label: 'May' },
+    { value: 6, label: 'June' },
+    { value: 7, label: 'July' },
+    { value: 8, label: 'August' },
+    { value: 9, label: 'September' },
+    { value: 10, label: 'October' },
+    { value: 11, label: 'November' },
+    { value: 12, label: 'December' },
+  ];
+
+  const handleChange = (event) => {
+    setSelectedMonth(Number(event.target.value)); 
+  };
+
+  return (
+    <div>
+      <label htmlFor="month-selector">Select Month: </label>
+      <select id="month-selector" value={selectedMonth} onChange={handleChange}>
+        {months.map((month) => (
+          <option key={month.value} value={month.value}>
+            {month.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default MonthSelector;
